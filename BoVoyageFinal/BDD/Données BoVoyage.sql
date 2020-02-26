bd6bd27ab12b1613615a1cb488891fd12a2c879b
jeremycailloux <jeremycailloux@gmail.com>
2020-02-26 12:07:54 +0100

    Ajout du .bak

---
+++ BoVoyageFinal/BDD/Données BoVoyage.sql
@@ -0,0 +1,178 @@
+﻿-- Afficher toutes les caractéristiques de la table Destination
+sp_help 'Destination'
+
+insert Destination(IdParente,
+Nom,
+Niveau, 
+Description)
+values (1, 'Guadeloupe', 3, 'Dans un site exceptionnel,en bordure d''un petit lagon turquoise, tout est réuni pour un séjour paradisiaque. Découvrez les merveilles de grande terre et de basse terre, les joies des plongées dans la réserve naturelle.')
+
+-- delete from Destination where Destination.Nom = 'Saint-Barthélémy'
+
+insert Destination(IdParente,
+Nom,
+Niveau, 
+Description)
+values (1, 'Saint-Barthélémy', 3, 'Imaginez une île où il fait 26 à 28 °C toute l''année. Baignez vous dans une eau turquoise.')
+
+insert Destination(IdParente,
+Nom,
+Niveau, 
+Description)
+values (2, 'Birmanie', 2, 'La Birmanie est un pays passionnant pour tous ceux qui s’intéressent à l''art, aux civilisations, à l''hindouisme. Ce pays s''ouvre et a conservé toute la richesse de son patrimoine culturel. Visitez les temples, les marchés, ...')
+
+insert Destination(IdParente,
+Nom,
+Niveau, 
+Description)
+values (3, 'Canada', 2, 'Découvrez la nature généreuse et les grandes villes du Canada en toute saison, grâce aux nombreux circuits que nous avons élaborés.')
+
+insert Destination(IdParente,
+Nom,
+Niveau, 
+Description)
+values (1, 'Bretagne', 3, 'Superbe région. Terre de légendes. De nombreux spots pour le surf et le kitesurf.')
+
+
+-- Afficher toutes les caractéristiques de la table Voyage
+
+sp_help 'Voyage'
+
+insert Voyage(IdDestination,
+DateDepart,
+DateRetour,
+PlacesDispo,
+PrixHT,
+Reduction,
+Descriptif)
+values (1, '03/07/2020', '03/14/2020', 10, 789.99, 0.30, 'Séjour d''une semaine en Guadeloupe')
+
+-- delete from Voyage where Voyage.IdDestination = 1
+
+insert Voyage(IdDestination,
+DateDepart,
+DateRetour,
+PlacesDispo,
+PrixHT,
+Reduction,
+Descriptif)
+values (2, '03/08/2020', '03/15/2020', 10, 999.99, 0.30, 'Séjour d''une semaine en Saint-Barthélémy')
+
+-- delete from Voyage where Voyage.IdDestination = 2
+
+insert Voyage(IdDestination,
+DateDepart,
+DateRetour,
+PlacesDispo,
+PrixHT,
+Reduction,
+Descriptif)
+values (3, '04/08/2020', '04/15/2020', 10, 999.99, 0.30, 'Séjour d''une semaine en Birmanie')
+
+-- delete from Voyage where Voyage.IdDestination = 3
+
+insert Voyage(IdDestination,
+DateDepart,
+DateRetour,
+PlacesDispo,
+PrixHT,
+Reduction,
+Descriptif)
+values (4, '04/07/2020', '04/14/2020', 10, 1099.99, 0.30, 'Séjour d''une semaine en Canada')
+
+-- delete from Voyage where Voyage.IdDestination = 4
+
+insert Voyage(IdDestination,
+DateDepart,
+DateRetour,
+PlacesDispo,
+PrixHT,
+Reduction,
+Descriptif)
+values (5, '05/08/2020', '05/15/2020', 10, 399.99, 0.30, 'Séjour d''une semaine en Bretagne')
+
+-- delete from Voyage where Voyage.IdDestination = 5
+
+
+-- Afficher toutes les caractéristiques de la table Personne
+sp_help 'Personne'
+
+insert Personne(
+TypePers,
+Civilite,
+Nom,
+Prenom,
+Email,
+Telephone,
+Datenaissance)
+values (1, 'M', 'Deniro', 'Robert', 'robert.deniro@gmail.com', '0662661236', '08/17/1943')
+
+insert Personne(
+TypePers,
+Civilite,
+Nom,
+Prenom,
+Email,
+Telephone,
+Datenaissance)
+values (2, 'F', 'Thurman', 'Uma', 'uma.thurman@gmail.com', '0668641276', '04/29/1970')
+
+
+-- Afficher toutes les caractéristiques de la table Client
+sp_help 'Client'
+
+insert Client(Id)
+values (1)
+
+-- Afficher toutes les caractéristiques de la table Voyageur
+sp_help 'Voyageur'
+
+insert Voyageur(Id,
+Idvoyage)
+values (1, 9)
+
+insert Voyageur(Id,
+Idvoyage)
+values (2, 9)
+
+-- Ici on a deux voyageurs qui ont voyagé ensemble pour le voyage  IdVoyage 9 et dont la réservation a été faite par le client  Id 1
+
+
+-- Afficher toutes les caractéristiques de la table DossierResa
+
+sp_help 'DossierResa'
+
+insert DossierResa (NumeroCB,
+IdClient,
+IdEtatDossier,
+IdVoyage)
+values ('4900560236488955', 1, 4, 9)
+
+-- Afficher toutes les caractéristiques de la table EtatDossier
+
+sp_help 'EtatDossier'
+
+insert EtatDossier(Id,
+Libelle)
+values (1, 'En attente')
+
+insert EtatDossier(Id,
+Libelle)
+values (2, 'En cours')
+
+insert EtatDossier(Id,
+Libelle)
+values (3, 'Refusée')
+
+insert EtatDossier(Id,
+Libelle)
+values (4, 'Acceptée')
+
+-- delete from EtatDossier where EtatDossier.Id = 1
+
+backup database BoVoyage to disk = N'C:\Users\Adminl\Desktop\BoVoyage\BDD\BoVoyage.bak' with format
+
+restore database BoVoyage from disk = N'C:\Users\Adminl\Desktop\BoVoyage\BDD\BoVoyage.bak'
+with
+move 'BoVoyage' to N'C:\Users\Adminl\Desktop\BoVoyage\BDD\BoVoyage.mdf',
+move 'BoVoyage_log' to N'C:\Users\Adminl\Desktop\BoVoyage\BDD\BoVoyage.ldf'
---
+++ BoVoyageFinal/BDD/Schema BoVoyage.sql
@@ -0,0 +1,150 @@
+-- Créer une base Essai avec les options par défaut
+CREATE DATABASE BoVoyage
+ ON  PRIMARY (NAME = BoVoyage, FILENAME = N'C:\Users\Adminl\Desktop\BoVoyage\BDD\BoVoyage.mdf')
+ LOG ON (NAME = BoVoyage_log, FILENAME = N'C:\Users\Adminl\Desktop\BoVoyage\BDD\BoVoyage.ldf')
+GO
+
+
+CREATE TABLE Client (
+	Id INTEGER NOT NULL
+)
+
+Go
+
+ALTER TABLE Client ADD Constraint Client_Pk PRIMARY KEY (Id)
+Go
+
+CREATE TABLE Destination 
+    ( Id INTEGER NOT NULL IDENTITY , 
+     IdParente INTEGER, 
+     Nom NVARCHAR (100) NOT NULL , 
+     Niveau TINYINT NOT NULL , 
+     Description NVARCHAR (500) 
+    )
+GO 
+
+
+
+EXEC sp_addextendedproperty 'MS_Description' , '1 : Continent
+2 : Pays
+3 : Région' , 'USER' , 'dbo' , 'TABLE' , 'Destination' , 'COLUMN' , 'Niveau'
+Go
+
+ALTER TABLE Destination ADD Constraint Destination_Pk PRIMARY KEY (Id)
+ Go
+
+CREATE TABLE Dossierresa 
+    ( Id INTEGER NOT NULL IDENTITY , 
+     NumeroCB VARCHAR (16) , 
+     IdClient INTEGER NOT NULL , 
+     IdEtatDossier TINYINT NOT NULL , 
+     IdVoyage INTEGER NOT Null ) 
+Go
+
+ALTER TABLE DossierResa ADD Constraint Dossierresa_Pk PRIMARY KEY (Id)
+
+Go
+
+CREATE TABLE Etatdossier 
+    (
+	Id        Tinyint NOT NULL,
+	Libelle   Nvarchar (30) NOT Null )
+Go
+
+ALTER TABLE EtatDossier ADD Constraint Etatdossier_Pk PRIMARY KEY (Id)
+
+Go
+
+CREATE TABLE Personne 
+ ( Id INTEGER NOT NULL IDENTITY , 
+	TypePers TINYINT NOT NULL , 
+	Civilite VARCHAR (3) NOT NULL , 
+	Nom NVARCHAR (30) NOT NULL , 
+	Prenom NVARCHAR (30) NOT NULL , 
+	Email NVARCHAR (30) NOT	Null,
+	Telephone VARCHAR ( 16 ),
+	Datenaissance DATE
+)
+GO
+
+ALTER TABLE Personne ADD Constraint Personne_Pk PRIMARY KEY (Id)
+Go
+
+CREATE TABLE Photo 
+    ( Id INTEGER NOT NULL IDENTITY , 
+     NomFichier NVARCHAR (100) NOT NULL , 
+     IdDestination INTEGER NOT Null )
+Go
+
+ALTER TABLE Photo ADD Constraint Photo_Pk PRIMARY KEY (Id)
+Go
+
+CREATE TABLE Voyage 
+    ( Id INTEGER NOT NULL IDENTITY , 
+     IdDestination INTEGER NOT NULL , 
+     DateDepart DATE NOT NULL , 
+     DateRetour DATE NOT NULL , 
+     PlacesDispo INTEGER NOT NULL , 
+     PrixHT DECIMAL (16,4) NOT NULL DEFAULT 0 , 
+     Reduction DECIMAL (3,2) NOT NULL , Descriptif Nvarchar(500) ) 
+Go
+
+ALTER TABLE Voyage ADD Constraint Voyage_Pk PRIMARY KEY (Id)
+ 
+Go
+
+CREATE TABLE Voyageur (
+	Id         INTEGER NOT NULL,
+	Idvoyage   INTEGER NOT NULL
+)
+
+Go
+
+ALTER TABLE Voyageur ADD Constraint Voyageur_Pk PRIMARY KEY (Id, Idvoyage)
+GO
+
+ALTER TABLE Client
+	ADD CONSTRAINT Client_Personne_Fk FOREIGN KEY ( Id )
+		REFERENCES Personne ( Id )
+Go
+
+ALTER TABLE Destination
+	ADD CONSTRAINT Destination_Destination_Fk FOREIGN KEY ( Idparente )
+		REFERENCES Destination ( Id )
+Go
+
+ALTER TABLE DossierResa
+	ADD CONSTRAINT Dossierresa_Client_Fk FOREIGN KEY ( Idclient )
+		REFERENCES Client ( Id )
+Go
+
+ALTER TABLE DossierResa
+	ADD CONSTRAINT Dossierresa_Etatdossier_Fk FOREIGN KEY ( Idetatdossier )
+		REFERENCES Etatdossier ( Id )
+Go
+
+ALTER TABLE DossierResa
+	ADD CONSTRAINT Dossierresa_Voyage_Fk FOREIGN KEY ( Idvoyage )
+		REFERENCES Voyage ( Id )
+Go
+
+ALTER TABLE Photo
+	ADD CONSTRAINT Photo_Destination_Fk FOREIGN KEY ( Iddestination )
+		REFERENCES Destination ( Id )
+Go
+
+ALTER TABLE Voyage
+	ADD CONSTRAINT Voyage_Destination_Fk FOREIGN KEY ( Iddestination )
+		REFERENCES Destination ( Id )
+Go
+
+ALTER TABLE Voyageur
+	ADD CONSTRAINT Voyageur_Personne_Fk FOREIGN KEY ( Id )
+		REFERENCES Personne ( Id )
+Go
+
+ALTER TABLE Voyageur
+	ADD CONSTRAINT Voyageur_Voyage_Fk FOREIGN KEY ( Idvoyage )
+		REFERENCES Voyage ( Id )
+Go
+
