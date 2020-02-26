1f9b7e4cb0c60d4e411cd1e98d7dbc9485901cae
Amine Cherif <cherif.bem@gmail.com>
2020-02-26 16:55:41 +0100

    Controller Personne

---
+++ BoVoyageFinal/Areas/SiteWeb/Controllers/PersonnesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BoVoyageFinal.Models;
+
+namespace BoVoyageFinal.Areas.SiteWeb.Controllers
+{
+    [Area("SiteWeb")]
+    public class PersonnesController : Controller
+    {
+        private readonly BoVoyageContext _context;
+
+        public PersonnesController(BoVoyageContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SiteWeb/Personnes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personne = await _context.Personne
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (personne == null)
+            {
+                return NotFound();
+            }
+
+            return View(personne);
+        }
+
+
+        // GET: SiteWeb/Personnes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personne = await _context.Personne.FindAsync(id);
+            if (personne == null)
+            {
+                return NotFound();
+            }
+            return View(personne);
+        }
+
+
+        private bool PersonneExists(int id)
+        {
+            return _context.Personne.Any(e => e.Id == id);
+        }
+    }
+}
