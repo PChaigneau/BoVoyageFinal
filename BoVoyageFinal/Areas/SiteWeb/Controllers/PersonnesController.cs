f4669c05440df7b009c17118aab13f3fc84861b8
Amine Cherif <cherif.bem@gmail.com>
2020-02-27 14:55:35 +0100

    Annulation

--- BoVoyageFinal/Areas/SiteWeb/Controllers/PersonnesController.cs
+++ BoVoyageFinal/Areas/SiteWeb/Controllers/PersonnesController.cs
@@ -54,6 +54,29 @@ namespace BoVoyageFinal.Areas.SiteWeb.Controllers
             return View(personne);
         }
 
+        // GET: SiteWeb/Personnes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SiteWeb/Personnes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TypePers,Civilite,Nom,Prenom,Email,Telephone,Datenaissance")] Personne personne)
+        {
+            personne.TypePers = 1; // Pour definir que c'est un client : TypePers =1.
+            if (ModelState.IsValid)
+            {
+                _context.Add(personne);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(personne);
+        }
+
 
         private bool PersonneExists(int id)
         {
