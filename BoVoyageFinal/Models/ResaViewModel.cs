e6c3aa6d136ca922ef9bb9507bb9c961aa885d79
Pierre <pichaigneau@laposte.net>
2020-02-28 17:40:53 +0100

    2802soir

---
+++ BoVoyageFinal/Models/ResaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+
+namespace BoVoyageFinal.Models
+{
+    public class ResaViewModel
+    {       
+        public Voyage Voyage { get; set; }
+
+        public Voyageur Voyageur { get; set; }
+
+    }
+}
