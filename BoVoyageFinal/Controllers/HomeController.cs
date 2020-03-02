94ce9307f5185b87a3be8d49ca07887ce5da9677
jeremycailloux <jeremycailloux@gmail.com>
2020-03-02 16:58:06 +0100

    17h00 Lundi 2

--- BoVoyageFinal/Controllers/HomeController.cs
+++ BoVoyageFinal/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using BoVoyageFinal.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Data.SqlClient;
 
 namespace BoVoyageFinal.Controllers
 {
@@ -27,15 +28,13 @@ namespace BoVoyageFinal.Controllers
             HomeVM vm = new HomeVM();
             vm.Top5Prix = _context.Voyage.Include(v => v.IdDestinationNavigation).ThenInclude(d => d.Photo).OrderBy(v => v.PrixHt).Take(5).ToList();
             vm.Top5DateDepart = _context.Voyage.Include(v => v.IdDestinationNavigation).ThenInclude(d => d.Photo).OrderBy(v => v.DateDepart).Take(5).ToList();
-            vm.Top5Destination = _context.Destination.Include(d => d.Photo).OrderBy(v => v.Nom).Take(5).ToList();
-            
-            /*
-            var destVoyage = _context.Destination.Include(d => d.Voyage);
-            if (destVoyage != null)
-            {
-                vm.Top5Destination = _context.Destination.Where(d => d.Voyage != null).Include(d => d.Photo).OrderBy(v => v.Nom).Take(5).ToList();
-            }
-           */
+            //vm.Top5Destination = _context.Destination.Include(d => d.Photo).OrderBy(v => v.Nom).Take(5).ToList();
+
+            vm.Top5Destination = _context.Destination.FromSqlRaw(@"select distinct top(5) d.Id,d.Nom, count(*) as IdParente, cast (0 as tinyint) as Niveau, '' as Description from Voyage v
+                                                                   inner join Destination d on d.Id = v.IdDestination
+                                                                   group by d.Id, d.Nom
+                                                                   order by Niveau desc").ToList(); 
+
             return View(vm);  
         }
 
@@ -50,6 +49,7 @@ namespace BoVoyageFinal.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
         [AllowAnonymous]
         public IActionResult Contact()
         {
